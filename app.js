75cfad4c538d4fd222fbf72195ce2a623e2d536d
Iraklis Georgas <igeorgas@culture.gr>
2017-07-26 16:59:25 +0300

    Playing with the framework, no actual work is done.... yet (≖ ͜ʖ≖)

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const routes = require('./routes');
+// import express from 'express'; // see: https://stackoverflow.com/questions/42645548/using-import-in-nodejs-server
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello motherfucker!!!');
+    res.send('Hello motherfucker!!!');
 });
 
+app.use('/foo', routes); // console.log "Hello" when visiting example.com/foo/users/foobar
 
 app.listen(port, () => console.log('Example app listening on port 3000!'));
