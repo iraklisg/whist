6e931924c3f17bc348e23a8205b2d653415a30fa
Iraklis Georgas <igeorgas@culture.gr>
2017-08-01 09:18:03 +0300

    Correct conflicts in app.js

--- app.js
+++ app.js
@@ -7,18 +7,14 @@ const nunjucks = require('nunjucks');
 
 const routes = require('./routes');
 const configServer = require('./config/server');
-const mongoose = require('mongoose');
 const configDB = require('./config/database');
 
-<<<<<<< HEAD
 // Create a new express app
 const app = express();
 
 // Set up mongoose connection (see http://mongoosejs.com/docs/connections.html#use-mongo-client)
 mongoose.connect(configDB.url);
-=======
 
-mongoose.connect('mongodb://127.0.0.1:27017/whist');
 // Schema Types
 let plainStringType = {type: String, required: false};
 let nicknameType = {type: String, match: /[\w-]+/, required: true, unique: true};
@@ -82,31 +78,21 @@ let Game = mongoose.model('Game', gameSchema);
 
 // });
 
-const app = express();
-
+// Template engine configuration
 nunjucks.configure('templates', {
     autoescape: true,
     express: app
 });
 
-const port = process.env.PORT || 8000;
-
+// End points ????
 app.get('/', (req, res) => {
     Player.find().exec(function(err, players){
         if (err) throw err;
         res.render('index.html', {players: players});
     });
 });
->>>>>>> 51d231037d1f24a0fbcfbfc4fe597dbcfe169d1c
 
-// End points ????
-app.get('/', (req, res) => res.send('Hello motherfuckers!!!'));
+app.get('/hello', (req, res) => res.send('Hello motherfuckers!!!'));
 app.use('/foo', routes); // console.log "Hello" when visiting example.com/foo/users/foobar
 
-<<<<<<< HEAD
 app.listen(configServer.port, () => console.log('Example app listening on port 3000!'));
-=======
-app.listen(port, () =>
-    console.log(`Example app listening on port ${port}!`)
-);
->>>>>>> 51d231037d1f24a0fbcfbfc4fe597dbcfe169d1c
