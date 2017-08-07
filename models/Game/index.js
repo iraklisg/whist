d44215443c40fe30a081dcde073d4c9650354ef9
Nick Mavrakis <mavrakis.n@gmail.com>
2017-08-07 15:33:06 +0300

    Updated the Game schema

--- models/Game/index.js
+++ models/Game/index.js
@@ -7,14 +7,13 @@ const mongoose = require('mongoose');
 
 const gameSchema = mongoose.Schema({
     place: String,
-    event: String,
     datetime: {type: Date},
     notes: String,
-    order: Array,
     players: [
         {
             player: {type: mongoose.Schema.Types.ObjectId, ref: 'Player', required: true},
             points: {type: Array, required: true},
+            order: {type: Number, required: true},
         }
     ],
 });
