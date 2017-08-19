bb9079794b35a83a6c86cc21eb5514d8a329bc03
Nick Mavrakis <mavrakis.n@gmail.com>
2017-08-19 19:13:10 +0300

    Created the /games/add route and HTML form for adding games. Fixed the navbar to work on mobile screens.

--- routes/games.js
+++ routes/games.js
@@ -5,8 +5,11 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const multer = require('multer');
 
 const {getGames, getGame, storeGame, putGame} = require('../controllers/games');
+const {getPlayers} = require('../controllers/players');
+let upload = multer();
 
 // create application/json parser
 const jsonParser = bodyParser.json();
@@ -28,24 +31,39 @@ router.get('/', async (req, res) => {
 });
 
 /**
-  * Get a game by id
+ * Go to page to create a new game
  */
-router.get('/:id', async (req, res) => {
+router.get('/add', async (req, res) => {
     try {
-        const game = await getGame(req.params.id);
-        return res.status(200).json(game);
+        const players = await getPlayers();
+        return res.render('games/add', {players: players});
     } catch (err) {
         return res.status(500).json({error: err.message});
     }
 });
 
 /**
- * Create a new game
+ * Handle form submission (creation of new game)
  */
-router.post('/', jsonParser, async (req, res) => {
+router.post('/add', async (req, res) => {
     try {
-        const newGame = await storeGame(req.body);
-        return res.status(200).json(newGame);
+        console.log(req.file);
+        console.log(req.body);
+        // const newGame = await storeGame(req.body);
+        const games = await getGames();
+        return res.render('games/add', {games: games});
+    } catch (err) {
+        return res.status(500).json({error: err.message});
+    }
+});
+
+/**
+  * Get a game by id
+ */
+router.get('/:id', async (req, res) => {
+    try {
+        const game = await getGame(req.params.id);
+        return res.status(200).json(game);
     } catch (err) {
         return res.status(500).json({error: err.message});
     }
